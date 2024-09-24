2aa8a5dafff06995e9da0f015e995ce4802d8d49
Rodolfo Olivieri <rolivier@redhat.com>
2024-09-24 13:48:47 -0300

    Update

--- ml_tech_challenge_phase3/__main__.py
+++ ml_tech_challenge_phase3/__main__.py
@@ -1,10 +1,11 @@
 # prompt: Now, generate the complete code for me with spotipy credentials variables and right scope
-
+import json
+import time
 import pickle
 import streamlit as st
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import numpy as np
+import pandas as pd
 
 # Create a Spotify client
 sp = spotipy.Spotify(
@@ -16,78 +17,121 @@ sp = spotipy.Spotify(
     )
 )
 
+tracks_with_audio_features = {}
+playlist_dict = {}
+
 MODEL = None
 with open("model.pkl", mode="rb") as f:
     MODEL = pickle.load(f)
 
-
-# Assuming you have a pre-trained model named 'model'
-# Replace this with your actual model loading and prediction code
-def predict_popularity(audio_features):
-    # Replace this with your actual prediction logic
-    # Example:
-    # input_features = np.array([audio_features['danceability'], audio_features['energy'], ...])
-    # popularity = model.predict([input_features])[0]
-    # return popularity
-    return 0.5  # Placeholder
-
-
 st.title("Spotify Playlist Viewer")
-
-audio_features_dict = {}  # Dictionary to store audio features
 
 try:
     playlists = sp.current_user_playlists()
     st.write("Your Playlists:")
 
-    playlist_options = []
-    for playlist in playlists["items"]:
-        playlist_options.append(playlist["name"])
+    playlist_options = [playlist["name"] for playlist in playlists["items"]]
 
     selected_playlists = st.multiselect(
-        "Select up to 4 playlists:", playlist_options, max_selections=4
+        "Select up to 2 playlists:", playlist_options, max_selections=2
     )
 
     if selected_playlists:
-        st.write(f"You selected: {', '.join(selected_playlists)}")
+        playlist_dict = {
+            playlist["id"]: {"name": playlist["name"], "tracks": []}
+            for playlist in playlists["items"]
+            if playlist["name"] in selected_playlists
+        }
 
-    for playlist_name in selected_playlists:
-        # Find the playlist ID based on the selected name
-        playlist_id = None
-        for playlist in playlists["items"]:
-            if playlist["name"] == playlist_name:
-                playlist_id = playlist["id"]
-                break
+    if playlist_dict:
+        for playlist_id, data in playlist_dict.items():
+            st.write(f"## Analisando playlist '{data['name']}'")
 
-        if playlist_id:
             try:
                 results = sp.playlist_items(playlist_id)
-                st.write(f"## Music in {playlist_name}: {len(results['items'])}")
-
-                with st.spinner("Extracting audio features..."):
-                    for item in results["items"]:
-                        track = item["track"]
-                        track_id = track["id"]
-                        # Extract audio features
-                        try:
-                            audio_features = sp.audio_features(track_id)[0]
-                            if audio_features:
-                                audio_features_dict[track_id] = audio_features
-                        except Exception as e:
-                            st.warning(
-                                f"Could not extract audio features for {track['name']}: {e}"
-                            )
+                playlist_dict[playlist_id]["tracks"] = results["items"]
             except Exception as e:
-                st.error(f"Error fetching music for {playlist_name}: {e}")
-        else:
-            st.warning(f"Could not find the playlist ID for {playlist_name}")
+                st.error(f"Error fetching music for {data['name']}: {e}")
 
-    if audio_features_dict:
-        st.write("## Audio Features:")
-        popularity_filter = st.radio(
+    if st.button("Extrair audio features"):
+        with st.spinner("Extraindo audio features..."):
+            track_ids = []
+            for item in playlist_dict.values():
+                tracks = item["tracks"]
+                track_ids = [track["track"]["id"] for track in tracks]
+
+            # Extract audio features
+            try:
+                time.sleep(0.2)
+                audio_features = sp.audio_features(track_ids)
+                for track_id, audio_feature in zip(track_ids, audio_features):
+                    tracks_with_audio_features[track_id] = [audio_feature]
+            except Exception as e:
+                st.warning(f"Could not extract audio features: {e}")
+
+            print(tracks_with_audio_features)
+
+    if tracks_with_audio_features:
+        st.write("## Filtro por popularidade:")
+        popularity_filter = st.selectbox(
             "Filter by Popularity:",
-            ["All", "High (70+)", "Medium (40-69)", "Low (0-39)"],
+            ["High (70+)", "Medium (40-69)", "Low (0-39)"],
         )
 
+        if popularity_filter:
+            if st.button("Criar playlist com novas músicas"):
+                new_playlist_name = st.text_input("Enter a name for your new playlist:")
+                new_playlist_description = st.text_input(
+                    "Enter the description of your new playlist:"
+                )
+
+                if new_playlist_name:
+                    filtered_tracks = []
+                    for track_id, audio_feature in tracks_with_audio_features.items():
+                        audio_feature_pd = pd.DataFrame(audio_feature)
+                        features = audio_feature_pd[
+                            [
+                                "duration_ms",
+                                "danceability",
+                                "energy",
+                                "key",
+                                "loudness",
+                                "mode",
+                                "speechiness",
+                                "acousticness",
+                                "instrumentalness",
+                                "liveness",
+                                "valence",
+                                "tempo",
+                                "time_signature",
+                            ]
+                        ]
+                        if popularity_filter == "High (70+)":
+                            popularity = MODEL.predict(features)[0]
+                            if popularity >= 70:
+                                filtered_tracks.append(track_id)
+                        elif popularity_filter == "Medium (40-69)":
+                            popularity = MODEL.predict(features)[0]
+                            if 40 <= popularity <= 69:
+                                filtered_tracks.append(track_id)
+                        elif popularity_filter == "Low (0-39)":
+                            popularity = MODEL.predict(features)[0]
+                            if popularity <= 39:
+                                filtered_tracks.append(track_id)
+
+                        user_id = sp.current_user()["id"]
+                        new_playlist = sp.user_playlist_create(
+                            user=user_id,
+                            name=new_playlist_name,
+                            description=new_playlist_description,
+                            public=False,
+                        )
+                        playlist_id = new_playlist["id"]
+
+                        # Add the filtered tracks to the new playlist
+                        sp.playlist_add_items(playlist_id, filtered_tracks)
+                        st.success(
+                            f"New playlist '{new_playlist_name}' created with filtered tracks!"
+                        )
 except Exception as e:
     st.error(f"Error fetching playlists: {e}")
