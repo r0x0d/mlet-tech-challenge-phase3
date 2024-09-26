983b8356be3831f72c93c0ffa879bec78c216a46
Rodolfo Olivieri <rodolfo.olivieri3@gmail.com>
2024-09-25 23:55:21 -0300

    Update project

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# mlet-tech-challenge-phase3
+# Machine Learning Engineering - Tech Challenge Phase 3
+
+## Clonando o projeto
+
+Para dar os primeiros passos, primeiro devemos clonar o projeto da seguinte maneira:
+
+```bash
+git clone git@github.com:r0x0d/mlet_tech_challenge_phase3.git
+```
+
+Com o projeto clonado, vamos entrar em sua pasta raíz e instalar as dependencias do projeto
+
+```bash
+cd mlet_tech_challenge_phase3
+virtualenv vev
+./bin/Scripts/activate.ps1
+pip install -r requirements.txt
+```
+
+```bash
+streamlit run mlet_tech_challenge_phase3/__main__.py
+```
+
+## Gerando o modelo para utilizar no app
+
+Primeiro, será necessário executar o notebook [Spotify API Dataset](./notebooks/Spotify_API_Dataset.ipynb) para baixar o dataset, tratar e limpar esses dados, e por fim, será possível trabalhar com a parte da modelagem.
+
+Após finalizado a execução do notebook acima, você deverá pegar o dataset limpo que é gerado em formato csv e utilzá-lo no notebook de modelagem, o [Sistema de Recomendação com API do Spotify](./notebooks/Sistema_de_recomendação_com_API_Spotify.ipynb). Ao final da execução de todas as células do notebook, um modelo em formato `.pkl` será gerado. 
+
+Coloque esse modelo na raíz desse repositório e então execute o app streamlit com o seguinte comando:
+
+```bash
+streamlit run mlet_tech_challenge_phase3/__main__.py
+```
--- ml_tech_challenge_phase3/__main__.py
+++ ml_tech_challenge_phase3/__main__.py
@@ -1,5 +1,3 @@
-# prompt: Now, generate the complete code for me with spotipy credentials variables and right scope
-import json
 import time
 import pickle
 import streamlit as st
@@ -17,24 +15,31 @@ sp = spotipy.Spotify(
     )
 )
 
-tracks_with_audio_features = {}
-playlist_dict = {}
 
-MODEL = None
-with open("model.pkl", mode="rb") as f:
-    MODEL = pickle.load(f)
+def load_model():
+    with open("model.pkl", mode="rb") as f:
+        model = pickle.load(f)
+        return model
 
-st.title("Spotify Playlist Viewer")
 
-try:
+def main():
+    st.title("Recomendação de músicas populares no Spotify!")
+
     playlists = sp.current_user_playlists()
-    st.write("Your Playlists:")
+    st.write("Suas playlists:")
 
     playlist_options = [playlist["name"] for playlist in playlists["items"]]
-
     selected_playlists = st.multiselect(
-        "Select up to 2 playlists:", playlist_options, max_selections=2
+        "Selecione até 4 playlists:", playlist_options, max_selections=4
     )
+    st.write("## Filtro por popularidade:")
+    popularity_filter = st.selectbox(
+        "Filtro por popularidade:",
+        ["Alta (70+)", "Média (40-69)", "Baixa (0-39)"],
+        index=1,
+    )
+
+    playlist_dict = {}
 
     if selected_playlists:
         playlist_dict = {
@@ -43,24 +48,20 @@ try:
             if playlist["name"] in selected_playlists
         }
 
-    if playlist_dict:
+    if st.button("Analisar playlsits"):
         for playlist_id, data in playlist_dict.items():
-            st.write(f"## Analisando playlist '{data['name']}'")
+            st.write(f"## Playlist analisada: '{data['name']}'")
 
-            try:
-                results = sp.playlist_items(playlist_id)
-                playlist_dict[playlist_id]["tracks"] = results["items"]
-            except Exception as e:
-                st.error(f"Error fetching music for {data['name']}: {e}")
+            results = sp.playlist_items(playlist_id)
+            playlist_dict[playlist_id]["tracks"] = results["items"]
 
-    if st.button("Extrair audio features"):
+        tracks_with_audio_features = {}
         with st.spinner("Extraindo audio features..."):
             track_ids = []
             for item in playlist_dict.values():
                 tracks = item["tracks"]
                 track_ids = [track["track"]["id"] for track in tracks]
 
-            # Extract audio features
             try:
                 time.sleep(0.2)
                 audio_features = sp.audio_features(track_ids)
@@ -69,69 +70,56 @@ try:
             except Exception as e:
                 st.warning(f"Could not extract audio features: {e}")
 
-            print(tracks_with_audio_features)
+        filtered_tracks = []
+        for (
+            track_id,
+            audio_feature,
+        ) in tracks_with_audio_features.items():
+            audio_feature_pd = pd.DataFrame(audio_feature)
+            features = audio_feature_pd[
+                [
+                    "duration_ms",
+                    "danceability",
+                    "energy",
+                    "key",
+                    "loudness",
+                    "mode",
+                    "speechiness",
+                    "acousticness",
+                    "instrumentalness",
+                    "liveness",
+                    "valence",
+                    "tempo",
+                    "time_signature",
+                ]
+            ]
+            model = load_model()
+            popularity = model.predict(features)[0]
+            if popularity_filter == "Alta (70+)":
+                if popularity >= 70:
+                    filtered_tracks.append(track_id)
+            elif popularity_filter == "Média (40-69)":
+                if 40 <= popularity <= 69:
+                    filtered_tracks.append(track_id)
+            elif popularity_filter == "Baixa (0-39)":
+                if popularity <= 39:
+                    filtered_tracks.append(track_id)
 
-    if tracks_with_audio_features:
-        st.write("## Filtro por popularidade:")
-        popularity_filter = st.selectbox(
-            "Filter by Popularity:",
-            ["High (70+)", "Medium (40-69)", "Low (0-39)"],
+        user_id = sp.current_user()["id"]
+        new_playlist = sp.user_playlist_create(
+            user=user_id,
+            name="Spotify Recommended Playlist - MLET",
+            description="Playlist criada com base em suas playlists.",
+            public=False,
+        )
+        playlist_id = new_playlist["id"]
+
+        # Add the filtered tracks to the new playlist
+        sp.playlist_add_items(playlist_id, filtered_tracks)
+        st.success(
+            "Nova playlist 'Spotify Recommended Playlist - MLET' criada com sucesso!"
         )
 
-        if popularity_filter:
-            if st.button("Criar playlist com novas músicas"):
-                new_playlist_name = st.text_input("Enter a name for your new playlist:")
-                new_playlist_description = st.text_input(
-                    "Enter the description of your new playlist:"
-                )
 
-                if new_playlist_name:
-                    filtered_tracks = []
-                    for track_id, audio_feature in tracks_with_audio_features.items():
-                        audio_feature_pd = pd.DataFrame(audio_feature)
-                        features = audio_feature_pd[
-                            [
-                                "duration_ms",
-                                "danceability",
-                                "energy",
-                                "key",
-                                "loudness",
-                                "mode",
-                                "speechiness",
-                                "acousticness",
-                                "instrumentalness",
-                                "liveness",
-                                "valence",
-                                "tempo",
-                                "time_signature",
-                            ]
-                        ]
-                        if popularity_filter == "High (70+)":
-                            popularity = MODEL.predict(features)[0]
-                            if popularity >= 70:
-                                filtered_tracks.append(track_id)
-                        elif popularity_filter == "Medium (40-69)":
-                            popularity = MODEL.predict(features)[0]
-                            if 40 <= popularity <= 69:
-                                filtered_tracks.append(track_id)
-                        elif popularity_filter == "Low (0-39)":
-                            popularity = MODEL.predict(features)[0]
-                            if popularity <= 39:
-                                filtered_tracks.append(track_id)
-
-                        user_id = sp.current_user()["id"]
-                        new_playlist = sp.user_playlist_create(
-                            user=user_id,
-                            name=new_playlist_name,
-                            description=new_playlist_description,
-                            public=False,
-                        )
-                        playlist_id = new_playlist["id"]
-
-                        # Add the filtered tracks to the new playlist
-                        sp.playlist_add_items(playlist_id, filtered_tracks)
-                        st.success(
-                            f"New playlist '{new_playlist_name}' created with filtered tracks!"
-                        )
-except Exception as e:
-    st.error(f"Error fetching playlists: {e}")
+if __name__ == "__main__":
+    main()
